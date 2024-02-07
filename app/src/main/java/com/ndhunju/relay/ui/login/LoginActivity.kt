68d182673fb37c0ee9c834aa9f9c2f952e7d18ed
ndhunju <dhunjunikesh@gmail.com>
2024-02-07 10:26:16 -0500

    When user is navigated to Main Screen from Login Screen, if user presses back, don't bring back to Login Screen. Instead exit the app.

--- app/src/main/java/com/ndhunju/relay/ui/login/LoginActivity.kt
+++ app/src/main/java/com/ndhunju/relay/ui/login/LoginActivity.kt
@@ -15,9 +15,15 @@ class LoginActivity: BaseActivity() {
             .commit()
 
         // If at any point in time, user is signed in, move to Main Screen
-        appStateBroadcastService.isUserSignedIn.observe(this) { isLoggedIn ->
-            if (isLoggedIn) {
-                startActivity(Intent(this, MainActivity::class.java))
+        appStateBroadcastService.isUserSignedIn.observe(this) { isSignedIn ->
+            if (isSignedIn) {
+                startActivity(Intent(this, MainActivity::class.java).apply {
+                    // When user presses back, don't bring them back to this activity
+                    flags = (Intent.FLAG_ACTIVITY_NEW_TASK
+                            or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                })
+                finish()
             }
 
         }
