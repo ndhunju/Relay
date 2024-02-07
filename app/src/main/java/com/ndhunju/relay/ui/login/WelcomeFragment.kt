e3d0b0662902620a4fc8787d45c87fdbb968b29e
ndhunju <dhunjunikesh@gmail.com>
2024-02-07 15:43:39 -0500

    Import R instead of android.R in WelcomeFragment.

--- app/src/main/java/com/ndhunju/relay/ui/login/WelcomeFragment.kt
+++ app/src/main/java/com/ndhunju/relay/ui/login/WelcomeFragment.kt
@@ -1,12 +1,12 @@
 package com.ndhunju.relay.ui.login
 
-import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import com.ndhunju.relay.R
 import com.ndhunju.relay.ui.account.AccountFragment
 import com.ndhunju.relay.ui.theme.RelayTheme
 
@@ -24,12 +24,12 @@ class WelcomeFragment: Fragment() {
                     WelcomeScreen(onClickNext = {
                         parentFragmentManager.beginTransaction()
                             .setCustomAnimations(
-                                com.ndhunju.relay.R.anim.enter_from_right,
-                                com.ndhunju.relay.R.anim.exit_to_left,
-                                com.ndhunju.relay.R.anim.enter_from_left,
-                                com.ndhunju.relay.R.anim.exit_to_right
+                                R.anim.enter_from_right,
+                                R.anim.exit_to_left,
+                                R.anim.enter_from_left,
+                                R.anim.exit_to_right
                             )
-                            .replace(R.id.content, AccountFragment.newInstance())
+                            .replace(android.R.id.content, AccountFragment.newInstance())
                             .addToBackStack(AccountFragment.TAG)
                             .commit()
                     })
