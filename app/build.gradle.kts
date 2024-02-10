5816bec782f5abda4ad880f30ceea24ef959acb6
ndhunju <dhunjunikesh@gmail.com>
2024-02-09 19:25:24 -0500

    Set custom splash screen so that it is not with white background even for dark theme.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -117,6 +117,9 @@ dependencies {
     // Datastore (Replaces SharedPreferences)
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
+    // Splash Screen
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
