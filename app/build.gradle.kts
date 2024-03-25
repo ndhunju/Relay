9c715c3e16d189a7589da017f0e09964149e63f1
ndhunju <dhunjunikesh@gmail.com>
2024-03-25 06:58:07 -0400

    Add App Check library so that the calls made to Firebase via Firebase SDK are authenticated with App Check tokens.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -141,10 +141,14 @@ dependencies {
     // Pull Down To Refresh
     implementation("androidx.compose.material:material:1.6.4")
 
+    // Add the dependencies for the App Check libraries. This library
+    // sends App Check tokens along with every request the app makes to Firebase
+    implementation("com.google.firebase:firebase-appcheck-playintegrity")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:32.7.4"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
