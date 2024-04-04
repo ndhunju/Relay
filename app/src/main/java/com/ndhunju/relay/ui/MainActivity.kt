3814ce6d0eb518af41c726965942af636c73d054
ndhunju <dhunjunikesh@gmail.com>
2024-04-03 20:11:14 -0400

    Move the logic for showing sms messages to MessageThreadFragment.

--- app/src/main/java/com/ndhunju/relay/ui/MainActivity.kt
+++ app/src/main/java/com/ndhunju/relay/ui/MainActivity.kt
@@ -3,73 +3,33 @@ package com.ndhunju.relay.ui
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.view.ViewGroup.LayoutParams
 import android.view.ViewTreeObserver
-import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ComposeView
-import androidx.lifecycle.lifecycleScope
 import com.ndhunju.relay.R
 import com.ndhunju.relay.RelayViewModelFactory
 import com.ndhunju.relay.ui.account.AccountFragment
-import com.ndhunju.relay.ui.custom.AppUpdateDialog
 import com.ndhunju.relay.ui.debug.DebugFragment
+import com.ndhunju.relay.ui.messages.MessageThreadFragment
 import com.ndhunju.relay.ui.messagesfrom.MessagesFromFragment
 import com.ndhunju.relay.ui.pair.PairWithParentFragment
 import com.ndhunju.relay.ui.pair.ShareEncryptionKeyWithQrCodeActivity
 import com.ndhunju.relay.ui.parent.ChildUserListFragment
-import com.ndhunju.relay.ui.theme.RelayTheme
-import com.ndhunju.relay.util.areSmsPermissionGranted
-import com.ndhunju.relay.util.checkIfSmsPermissionsGranted
-import com.ndhunju.relay.util.extensions.getAppVersionCode
-import com.ndhunju.relay.util.extensions.startActivityForUrl
-import com.ndhunju.relay.util.requestSmsPermission
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 
 class MainActivity : BaseActivity() {
 
     // Member Variables
     private val viewModel: MainViewModel by viewModels { RelayViewModelFactory }
 
-    private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestMultiplePermissions()
-    ) { permissions ->
-        if (areSmsPermissionGranted(permissions)) {
-            viewModel.onSmsPermissionGranted()
-        } else {
-            // Permissions denied
-            viewModel.onSmsPermissionDenied()
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            RelayTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MainScreen(viewModel)
-                }
-            }
-        }
-
-        showSplashScreenUntilReady()
-        checkForMinimumVersionCode()
-
         viewModel.setTitle(getString(R.string.app_name))
+        showSplashScreenUntilReady()
+        bindNavigationCallbacks()
+        // Add MessageThreadFragment immediately
+        MessageThreadFragment.addToContent(supportFragmentManager)
+    }
 
-        viewModel.doRequestSmsPermission = {
-            requestSmsPermission(requestPermissionLauncher)
-        }
+    private fun bindNavigationCallbacks() {
 
         viewModel.doOpenPairWithParentFragment = {
             PairWithParentFragment.addToContent(supportFragmentManager)
@@ -104,14 +64,6 @@ class MainActivity : BaseActivity() {
             DebugFragment.addToContent(supportFragmentManager)
         }
 
-        // Check if SMS read and send permissions are granted
-        if (checkIfSmsPermissionsGranted(this)) {
-            viewModel.onSmsPermissionGranted()
-        } else {
-            // if (shouldShowRequestPermissionRationale(this, READ_SMS))
-            // Always show the rationale
-            viewModel.onSmsPermissionDenied()
-        }
     }
 
     /**
